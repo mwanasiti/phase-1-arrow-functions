dc88fa860b9309d266f60188d769150648cda74b
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-12 21:14:42 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+//let foo=()=>{
+    //return 'bar';
+  //}
+
+  //const difference =()=> 10 - 1;
+
+  //const nums = [1,2,3];
+//const squares = nums.map(x => x ** 2);
+
+//lapsedUserAccounts.map( u => sendBillTo(u.address) );
+
+const divide = () => 2000/100
+const square = y => y * y;
+const add = (a, b) =>(a+b);
